c06cdf7ab7dba2bfc484db58e2c2cbbdc82ff5a1
Scott Stensland <scottstensland@gmail.com>
2020-11-25 12:10:48 -0500

    modified:   notes 	modified:   server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World its code from 20201125  about noon ish');
 });
 
 app.listen(PORT, HOST);
